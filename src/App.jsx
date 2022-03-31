6441195c6c94b522c0d7e5477ee62ee837061514
Thomas Ochman <thomas@dragster.se>
2022-03-31 10:41:55 +0200

    cleans up unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import Articles from './Components/Articles.jsx'
-import { Routes, Route } from 'react-router-dom'
-import Article from './Components/Article.jsx'
+import React from "react";
+import Articles from "./Components/Articles.jsx";
+import { Routes, Route } from "react-router-dom";
+import Article from "./Components/Article.jsx";
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Articles />} />
-        <Route path="/article/Deep%20Work" element={<Article/>} />
-      </Routes>
-    </div>
-  )
-}
-export default App
+    <Routes>
+      <Route path="/" element={<Articles />} />
+      <Route path="/article/:id" element={<Article />} />
+    </Routes>
+  );
+};
+export default App;
