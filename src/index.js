f64abbef46e1802b5c33ab31eea9588839ee99b1
Thomas Ochman <thomas@dragster.se>
2022-04-01 10:29:51 +0200

    adds and configures redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import configureStore from './state/store/configureStore'
 
+const store = configureStore()
+
+window.store = store
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/state/store/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import initialState from "./initialState";
+import rootReducer from '../reducers/rootReducer'
+
+const configureStore = () => {
+  return createStore(rootReducer, initialState)
+}
+
+export default configureStore
