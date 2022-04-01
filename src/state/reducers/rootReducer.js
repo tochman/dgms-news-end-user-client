da1eba43c27b362078bddb3c21ffee4e8ad79d92
Thomas Ochman <thomas@dragster.se>
2022-04-01 11:09:37 +0200

    chages conditional i rootReducer

--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,16 +1,30 @@
 const rootReducer = (state, action) => {
-  if (action.type === "SET_ARTICLES") {
-    return {
+  // if (action.type === "SET_ARTICLES") {
+  //   return {
+  //     ...state,
+  //     articles: action.payload,
+  //   };
+  // } else if (action.type === "SET_ACTIVE_ARTICLE") {
+  //   return {
+  //     ...state,
+  //     activeArticle: action.payload
+  //   }
+  // } else {
+  //   return { ...state };
+  // }
+  switch (action.type) {
+    case "SET_ARTICLES":
+      return {
       ...state,
       articles: action.payload,
     };
-  } else if (action.type === "SET_ACTIVE_ARTICLE") {
-    return {
+    case "SET_ACTIVE_ARTICLE":
+          return {
       ...state,
       activeArticle: action.payload
     }
-  } else {
-    return { ...state };
+    default:
+      return { ...state };
   }
 };
 
