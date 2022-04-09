aa5b0d9b257ccb34a7bb221992acc31856b95a53
gsim1903 <gavin@internetbusker.co>
2022-04-09 11:20:05 +0200

    authentication happy path tested

--- cypress/integration/userCanSeeSingleArticle.feature.js
+++ cypress/integration/userCanSeeSingleArticle.feature.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
-describe('Visitor can', () => {
-  describe('see a single article when atuhenticated', () => {
-    before(() => {
+describe('Visitor can see articles appropriate to their authorisation', () => {
+
+  describe('see a single article when authenticated', () => {
+    beforeEach(() => {
       cy.intercept('GET', 'api/articles', {
         fixture: 'articles.json',
       }).as('getArticles')
@@ -10,7 +11,7 @@ describe('Visitor can', () => {
         fixture: 'articleShow.json',
       }).as('getSingleArticle')
 
-      cy.intercept('POST', 'api/auth/sign_in', {
+      cy.intercept('POST', '/api/auth/sign_in', {
         fixture: 'authenticationSuccess.json',
         headers: { uid: 'user@email.com' },
       })
@@ -21,36 +22,57 @@ describe('Visitor can', () => {
 
       cy.visit('/')
 
-      cy.get('[data-cy=login-button]').click()
+      cy.get('[data-cy=show-button]').first().click()
+
       cy.get('[data-cy=login-email]').type('username')
       cy.get('[data-cy=login-password]').type('password')
       cy.get('[data-cy=login-submit-button]').click()
-
-      
     })
+  
 
-    it('is expected to display a sign in button', () => {
-      cy.get('[data-cy=login-button]').should("be.visible")
-    })
-
-    it('is expected to inform user that login was successful', () => {
-      cy.get('[data-cy=flash-message]').should("contain.text", "Login sucessful")
-    })
-
-
-
-    it('is expected to display correct url', () => {
-      cy.get('[data-cy=show-button]').first().click()
-      cy.url().should('contain', 'http://localhost:3000/article/1')
-    })
-
-    it('is expected to display the body of the article', () => {
-      cy.get('[data-cy=article-body]').should(
-        'contain.text',
-        'Lorem ipsum dolor',
-      )
-    })
+  it('is expected to display a sign in button', () => {
+    cy.get('[data-cy=login-button]').should('be.visible')
   })
 
-  describe('can not see a single article in full when unauthenticated', () => {})
+  it('is expected to inform user that login was successful', () => {
+    cy.get('[data-cy=flash-message]').should(
+      'contain.text',
+      'Login successful'
+      )
+  })
+
+  it('is expected to display correct url', () => {
+    cy.url().should('contain', 'http://localhost:3000/article')
+  })
+
+  it('is expected to display the body of the article', () => {
+    cy.get('[data-cy=article-body]').should(
+      'contain.text', 
+      'Lorem ipsum dolor'
+      )
+  })
+})
+
+
+
+// describe('can not see a single article in full when unauthenticated', () => {})
+// before(() => {
+//   cy.intercept('GET', 'api/articles', {
+//     fixture: 'articles.json',
+//   }).as('getArticles')
+
+//   cy.visit('/')
+
+//   cy.get('[data-cy=show-button]').first().click()
+
+//   it('is expected to request the user to sign in', () => {
+//     cy.get('[data-cy=login-requesst]').should(
+//       'container.text',
+//       'Please login to view full article',
+//     )
+//   })
+
+//   it('is expected to redirect user to login screen', () => {
+//     cy.url().should('contain', 'http://localhost:3000/login')
+//   })
 })
