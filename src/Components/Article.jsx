c3f906ada4ef3b109e4e22944ef6b6600b09f1c8
shagufta <shagufta.batool8@gmail.com>
2022-04-11 03:17:39 +0200

    refactored Article component

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,53 +1,50 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { Card, Container } from 'semantic-ui-react'
-import { useParams, useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Card, Container } from "semantic-ui-react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import Login from "./Login";
 
 const Article = ({ toast }) => {
   let navigate = useNavigate();
-  let { id } = useParams()
-  const dispatch = useDispatch()
-  const { activeArticle, userAuthenticated } = useSelector((state) => state)
-  let article = activeArticle
+  let { id } = useParams();
+  const dispatch = useDispatch();
+  const { activeArticle, userAuthenticated } = useSelector((state) => state);
+  let article = activeArticle;
 
   const fetchArticle = async () => {
-    const response = await axios.get(`api/article/${id}`)
-    dispatch({ type: 'SET_ACTIVE_ARTICLE', payload: response.data.article })
-    if (!userAuthenticated) {
-      toast.error("Please login to view full articles");
-    }
-  }
+    const response = await axios.get(`api/article/${id}`);
+    dispatch({ type: "SET_ACTIVE_ARTICLE", payload: response.data.article });
+    // if (!userAuthenticated) {
+    //   toast.error("Please login to view full articles");
+    // }
+  };
 
   useEffect(() => {
-    fetchArticle()
+    fetchArticle();
   }, []);
   useEffect(() => {
     if (!userAuthenticated) {
+      toast.error("Please login to view full articles");
       navigate("/login");
     }
   }, []);
 
   return (
     <Container text>
-      {article && userAuthenticated ? (
-        <Card
-          header={article.title}
-          meta={`By: ${article.author}`}
-          image={article.image}
-          description={() => (
-            <>
-              <h2>{article.headline}</h2>
-              <p data-cy="article-body">{article.body}</p>
-            </>
-          )}
-        ></Card>
-      ) : (
-        <Login toast={toast} />
-      )}
+      <Card
+        header={article?.title}
+        meta={`By: ${article?.author}`}
+        image={article?.image}
+        description={() => (
+          <>
+            <h2>{article?.headline}</h2>
+            <p data-cy="article-body">{article?.body}</p>
+          </>
+        )}
+      ></Card>
     </Container>
   );
-}
+};
 
-export default Article
+export default Article;
