763bbe5a8470721d95931ffe6dcabdc50d2f5fab
maxarvid <max.arvid.anderson@gmail.com>
2022-04-12 10:39:09 +0200

    unnests describe blocks in authenitaction spec

--- cypress/integration/userCanAccessArticleByAuthenticating.feature.js
+++ cypress/integration/userCanAccessArticleByAuthenticating.feature.js
@@ -1,25 +1,22 @@
 describe("visitor can see an single article by authenticating right away", () => {
-  
-  describe("visitor is able and required to authenticate", () => {
-    beforeEach(() => {
-      cy.intercept("GET", "api/articles", {
-        fixture: "articles.json",
-      }).as("getArticles");
-      cy.intercept("GET", "**/article/**", {
-        fixture: "articleShow.json",
-      }).as("getSingleArticle");
+  beforeEach(() => {
+    cy.intercept("GET", "api/articles", {
+      fixture: "articles.json",
+    }).as("getArticles");
+    cy.intercept("GET", "**/article/**", {
+      fixture: "articleShow.json",
+    }).as("getSingleArticle");
 
-      cy.intercept("POST", "api/auth/sign_in", {
-        fixture: "authenticationSuccess.json",
-        headers: { uid: "user@email.com" },
-      });
-      cy.intercept("GET", "api/auth/validate_token**", {
-        fixture: "authenticationSuccess.json",
-      });
-
-      cy.visit("/");
+    cy.intercept("POST", "api/auth/sign_in", {
+      fixture: "authenticationSuccess.json",
+      headers: { uid: "user@email.com" },
     });
-
+    cy.intercept("GET", "api/auth/validate_token**", {
+      fixture: "authenticationSuccess.json",
+    });
+    cy.visit("/");
+  });
+  describe("visitor is able and required to authenticate", () => {
     it("is expected to display a sign in button", () => {
       cy.get("[data-cy=login-button]").should("be.visible");
     });
@@ -31,34 +28,39 @@ describe("visitor can see an single article by authenticating right away", () =>
         "Please login to view full articles"
       );
     });
+  });
+  describe("logging in right away", () => {
+    beforeEach(() => {
+      cy.get("[data-cy=login-button]").click();
+      cy.get("[data-cy=login-email]").type("user@email.com");
+      cy.get("[data-cy=login-password]").type("password");
+      cy.get("[data-cy=submit-button]").click();
+    });
 
-
-    describe("logging in right away", () => {
+    it("is expected to redirect user to all articles", () => {
+      cy.get("[data-cy=articles-list]").children().should("have.length", 6);
+    });
+    it("is expected to inform user that login was successful", () => {
+      cy.get("[data-cy=flash-message]").should(
+        "contain.text",
+        "Login successful"
+        );
+      });
+    });
+    describe("By clicking an article and then logging in", () => {
       beforeEach(() => {
-        cy.get("[data-cy=login-button]").click();
+        cy.get("[data-cy=head-lines]").first().click();
+      });
+  
+      it("is expected to redirect visitor to login screen", () => {
+        cy.url().should("eq", "http://localhost:3000/login");
+      });
+  
+      it("is expected user to access full article after logging in", () => {
         cy.get("[data-cy=login-email]").type("user@email.com");
         cy.get("[data-cy=login-password]").type("password");
         cy.get("[data-cy=submit-button]").click();
-      });
-
-      it("is expected to redirect user to all articles", () => {
-        cy.get("[data-cy=articles-list]").children().should("have.length", 6);
-      });
-      it("is expected to inform user that login was successful", () => {
-        cy.get("[data-cy=flash-message]").should(
-          "contain.text",
-          "Login successful"
-        );
-      });
-
-      describe("By clicking an article and then logging in", () => {
-        beforeEach(() => {
-          cy.get("[data-cy=head-lines]").first().click();
-        });
-        it("is expected user to access full article after logging in right away", () => {
-          cy.url().should("eq", "http://localhost:3000/article/1");
-        });
+        cy.url().should("eq", "http://localhost:3000/article/1");
       });
     });
-  });
 });
