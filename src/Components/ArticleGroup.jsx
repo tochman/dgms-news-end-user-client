a2f51b2f52cdb411dc370541cdf581d2c9986a34
gsim1903 <gavin@internetbusker.co>
2022-04-13 08:01:14 +0200

    returns user location using open cage

--- src/Components/ArticleGroup.jsx
+++ src/Components/ArticleGroup.jsx
@@ -1,38 +1,37 @@
-import React from "react";
-import { useParams, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Container, Card } from "semantic-ui-react";
+import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Container, Card } from 'semantic-ui-react'
 
 const ArticleGroup = () => {
-  const { category } = useParams();
-  const { articles } = useSelector((state) => state);
+  const { category } = useParams()
+  const { articles } = useSelector((state) => state)
 
   const articlesList = articles[category]?.map((article) => {
     return (
-      <Card>
-      <li key={article.id} style={{ listStyleType: "none" }}>
-        <Link to={`/article/${article.id}`}>
-          <div>
-            <h1 data-cy="article-title">{article.title}</h1>{" "}
-          </div>
-          <img 
-            src={article.image}
-            alt=""
-            style={{ height: 200 + "px", width: "auto" }}
-            
-          />
-        </Link>
+      <li key={article.id} style={{ listStyleType: 'none' }}>
+        <Card>
+          <Link to={`/article/${article.id}`}>
+            <div>
+              <h1 data-cy="article-title">{article.title}</h1>{' '}
+            </div>
+            <img
+              src={article.image}
+              alt=""
+              style={{ height: 200 + 'px', width: 'auto' }}
+            />
+          </Link>
+        </Card>
       </li>
-      </Card>
-    );
-  });
+    )
+  })
 
   return (
     <Container text>
       <div data-cy="category_header"> The latest {category} news </div>
       <ul data-cy="articles-list">{articlesList}</ul>
     </Container>
-  );
-};
+  )
+}
 
-export default ArticleGroup;
+export default ArticleGroup
--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Location = () => {
-  return (
-    <div
-    data-cy="user-location"
-    >Sweden</div>
-  )
+  const [userInfo, setUserInfo] = useState({ city: ''})
+
+  const fetchUserCoordinates = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject, {
+        enableHighAccuracy: true,
+      })
+    })
+  }
+  const fetchGeolocation = async () => {
+    const position = await fetchUserCoordinates()
+    const openCageResponse = await axios.get(
+      'https://api.opencagedata.com/geocode/v1/json',
+      {
+        params: {
+          key: "173d229274d946708766616770f94b87",
+          q: `${position.coords.latitude}+${position.coords.longitude}`,
+        },
+      },
+    )
+    setUserInfo({ country: openCageResponse.data.results[0].components.country })
+  }
+
+  useEffect(() => {
+    fetchGeolocation()
+  }, [])
+
+  return <div data-cy="user-location">{userInfo.country}</div>
 }
 
 export default Location
